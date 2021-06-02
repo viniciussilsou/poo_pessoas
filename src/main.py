a99aeb2452013a265a8e01865a05f0df7141a0f6
fatimasilvaed <fatimasilvaed@gmail.com>
2021-06-02 19:23:33 -0300

    Criação de classes, models e main

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ nova_pessoa = pessoa_manager.criar_pessoa(cpf, nome, idade, telefone)
 
 pessoa_buscada = pessoa_manager.buscar_pessoa(cpf)
 
-pessoa_alterar = pessoa_manager.atualizar_pessoa(cpf, "NUNO", 50, 343444343)
+pessoa_alterar = pessoa_manager.atualizar_pessoa(cpf, "fulano", 50, 343444343)
 pessoa_buscada = pessoa_manager.buscar_pessoa(cpf)
 
 nova_pessoa = pessoa_manager.criar_pessoa(3344433434, "teste", 56, 355546667)
